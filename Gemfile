8e213ade0f9f73cc48e426e9c22c2f204e0aaf8a
Michelle Houle <houle.m@husky.neu.edu>
2018-06-03 21:24:08 -0400

    built two pages, added scss, built navbar

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-reloader'
+gem 'sinatra-partial'
+gem 'sinatra-contrib'
+
+gem 'haml'
+gem 'sass'
--- website.rb
+++ website.rb
@@ -1,8 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/partial'
+require 'sinatra/namespace'
 require 'haml'
+require 'sass'
 
 get '/' do
   haml :index
+end
+
+namespace '/game_jams' do
+  get '/dopey_doughy_boi' do
+    haml :'game_jams/dopey_doughy_boi'
+  end
+  get '/uncanny_valley' do
+    haml :'game_jams/uncanny_valley'
+  end
 end
